fb23660a5ac8dd62ae6f8530490262ec54edb4f8
Erin <erinpeifer@gmail.com>
2023-01-12 18:50:45 -0600

    when search btn is clicked api will request geocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,63 @@
 var key = "2e72e53f481f6b9de8ec80bdf19fe4dd" 
 var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct';
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast';
+var weatherUrl = 'http://api.openweathermap.org/data/2.5/';
 var storedSearchTerm = localStorage.getItem("Search Term");
 var storedState = localStorage.getItem("State")
-
+var temp = "";
+var wind = "";
+var humidity = "";
+var city = "";
+var long = "";
+var lat = "";
+var state = "";
 
 // not running current search it's running what was previously in local storage
 
-$(".search-btn").on("click", function (event){
-    var searchTerm = $(this).parent().find(".search-term").val();
+function requestForecast(){
+    var searchTerm = $(".search-term").val();
     localStorage.setItem("Search Term", searchTerm);
 
-    fetch(geoUrl + "?q=" + storedSearchTerm + "," + storedState + ",US&limit=5&appid=" + key)
-    .then(function (response) {
-        return response.json();
-    }) 
-    .then (function(data) {
+    fetch(weatherUrl + "weather?q=" + storedSearchTerm + ",US&units=imperial&appid=" + key)
+    .then((response) => 
+        response.json())
+
+    .then ((data) => {
         console.log(data);
-    //     // saveLatLong(data);
-    });
+        temp = data.main.temp;
+        wind = data.wind.speed;
+        humidity = data.main.humidity;
+// displayFeature()
+})
+  }
+
+    // function displayFeature() {
+    //     $("#featured-city").text(name + ", " + storedState)
+    //     $('#featured-temp').text("Temp: " + temp + "F")
+    //     $('#featured-wind').text("Wind: " + wind + "mph")
+    //     $('#featured-humidity').text("Humidity: " + humidity + "%")
+    // }
+
+  $(".search-btn").on("click", function (event){
+    var searchTerm = $(".search-term").val();
+    localStorage.setItem("Search Term", searchTerm);
+
+    fetch(geoUrl + "?q=" + storedSearchTerm + ","+ storedState + ",US&appid=" + key)
+    .then((response) => 
+        response.json())
+
+    .then ((data) => {
+        console.log(data);
+        long = data[0].lon;
+        lat = data[0].lat;
+        city = data[0].name;
+        state = data[0].state;
+    })
+    requestForecast()
+
   })
 
-//   function saveLatLong (data) {
-//     var searchLat = data.lat;
-//    console.log(searchLat)
-//   }
 
-var states = [
-    'Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'
-    ]
+var states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 
     var stateList = $('.dropdown-menu')
     $.each(states, function(i) {
@@ -49,4 +78,3 @@ var states = [
 // eventually for search history?
     // var currentSearch = searchTerm + ", " + selectedState
     // console.log(currentSearch)
-
