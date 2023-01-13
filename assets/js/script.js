3efd0a073dfeccdd3b85617b0f79c863227fe929
Erin <erinpeifer@gmail.com>
2023-01-12 23:08:43 -0600

    creates and populates forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct';
 var weatherUrl = 'http://api.openweathermap.org/data/2.5/';
 var storedSearchTerm = localStorage.getItem("Search Term");
 var storedState = localStorage.getItem("State")
-var temp = "";
-var wind = "";
-var humidity = "";
+// var temp = "";
+// var wind = "";
+// var humidity = "";
 var city = "";
 var state = "";
-let divEl = $('#div')
+let divEl = $('#forecast')
 
-
+// not running current search it's running what was previously in local storage
 $(".search-btn").on("click", function (event) {
     var searchTerm = $(".search-term").val();
     localStorage.setItem("Search Term", searchTerm);
@@ -25,18 +25,22 @@ $(".search-btn").on("click", function (event) {
             let lat = data[0].lat;
             city = data[0].name;
             state = data[0].state;
+            requestCurrentWeather(lat, long)
             requestForecast(lat, long)
         })
 
-
 })
+
+function requestCurrentWeather(lat, long) {
+    fetch(weatherUrl + )
+}
 
 
 function requestForecast(lat, long) {
     var searchTerm = $(".search-term").val();
     localStorage.setItem("Search Term", searchTerm);
 
-    fetch(weatherUrl + "forecast?lat=" + lat + "&lon=" + long + "&unit=imperial&appid=" + key)
+    fetch(weatherUrl + "forecast?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + key)
         .then((response) =>
             response.json())
 
@@ -44,13 +48,44 @@ function requestForecast(lat, long) {
             console.log(data);
             var daysIndex = [0, 8, 16, 24, 32]
             for (var i = 0; i < daysIndex.length; i++) {
-                var forecastArr = data.list[daysIndex[i]]
+                var forecastArr = data.list[daysIndex[i]];
                 console.log(forecastArr)
-                let div = $('<div>')
+                var card = $('<div>');
+                var day = $('<h3>');
+                var content = $('<ul>');
+                var tempEl = $('<li>');
+                var windEl = $('<li>');
+                var humidityEl = $('<li>');
+                var date = forecastArr.dt_txt;
+                var temp = forecastArr.main.temp_min + " - " + forecastArr.main.temp_max;
+                var wind = forecastArr.wind.speed;
+                var humidity = forecastArr.main.humidity;
 
-                div.text(forecastArr.main.temp)
 
-                divEl.append(div)
+                card.attr("class", "col-12 card");
+                day.attr("class", "card-header");
+                day.attr("id", "day");
+                content.attr("class", "list-group list-group-flush");
+                tempEl.attr("class", "list-group-item");
+                windEl.attr("class", "list-group-item");
+                humidityEl.attr("class", "list-group-item");
+                divEl.append(card);
+                card.append(day);
+                day.append(date);
+                card.append(content);
+                content.append(tempEl)
+                tempEl.text("Temp: " + temp + "F");
+                content.append(windEl)
+                windEl.text("Wind: " + wind + "mph");
+                content.append(humidityEl)
+                humidityEl.text("Humidity: " + humidity + "%");
+
+
+                // div.attr(".col-12 card")
+                // divEl.append(div)
+                // div.text(forecastArr.main.temp + "F")
+
+                // divEl.append(div)
                 // create div  
             }
             // displayForecast(forecastArr)
@@ -58,7 +93,17 @@ function requestForecast(lat, long) {
         })
 }
 
+// function displayForecast(forecastArr) {
+//     // 
 
+//     let temp = forecastArr.main.temp
+//     // append to 
+//     console.log(temp)
+// $("#featured-city").text(name + ", " + storedState)
+// $('#featured-temp').text("Temp: " + temp + "F")
+// $('#featured-wind').text("Wind: " + wind + "mph")
+// $('#featured-humidity').text("Humidity: " + humidity + "%")
+//}
 
 
 
@@ -84,14 +129,3 @@ $('.dropdown-item').on('click', function () {
 // eventually for search history?
     // var currentSearch = searchTerm + ", " + selectedState
     // console.log(currentSearch)
-    // function displayForecast(forecastArr) {
-//     //
-
-//     let temp = forecastArr.main.temp
-//     // append to
-//     console.log(temp)
-    // $("#featured-city").text(name + ", " + storedState)
-    // $('#featured-temp').text("Temp: " + temp + "F")
-    // $('#featured-wind').text("Wind: " + wind + "mph")
-    // $('#featured-humidity').text("Humidity: " + humidity + "%")
-//}
